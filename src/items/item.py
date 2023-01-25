f5b63666643b85704cf620b6c6e85d1b85a6225b
Bernbark <korystennett@yahoo.com>
2023-01-24 17:24:49 -0800

    Added randomize functions for drinks and food. Added functionality to shop menus to delete them once used. A user can only shop once from each of the dropdowns per shop visit.

--- src/items/item.py
+++ src/items/item.py
@@ -1,9 +1,34 @@
+import random
+from enum import Enum
+
+
+class Rarity(Enum):
+    COMMON = 1
+    UNCOMMON = 2
+    RARE = 3
+    EPIC = 4
+    LEGENDARY = 5
+
+
+def assign_rarity():
+    randomNum = random.randint(1,5)
+    for data in Rarity:
+        if randomNum == data.value:
+            return data
+
 class Item:
     """
     Base class for Items, from here we build up and make food, drinks, armor, and weapons
     """
     def __init__(self, name: str = "Default Item", cost: int = 999, description: str = "Unknown"):
-        # self.rarity = SOME RANDOM RARITY
+        self.rarity = assign_rarity()
         self.name = name
         self.cost = cost
         self.description = description
+
+    def set_cost(self, cost):
+        self.cost = cost
+
+    def set_description(self, description):
+        self.description = description
+
--- src/items/randomizeItem.py
+++ src/items/randomizeItem.py
@@ -1,9 +1,51 @@
+import random
+
+from src.items.food import Food
+from src.items.drink import Drink
+
 foodNames = [
     "Banana",
     "Apple",
     "Pizza"
 ]
 
+drinkNames = [
+    "Smoothie",
+    "Alcohol",
+    "Soda"
+]
+
+MIN_COST = 100
+MAX_COST = 10000
+
+MINIMUM_FOOD_HEALING = 100
+MAX_FOOD_HEALING = 500
+
+MIN_STAMINA_HEALING = 5
+MAX_STAMINA_HEALING = 20
+
 
 def randomize_food():
-    pass
+    name = random.choice(foodNames)
+    cost = random.randint(MIN_COST, MAX_COST)
+    healing = random.randint(MINIMUM_FOOD_HEALING, MAX_FOOD_HEALING)
+    stamina = random.randint(MIN_STAMINA_HEALING, MAX_STAMINA_HEALING)
+    food = Food(name, cost, "No description properly serves this right.", healing, stamina)
+    if food.rarity.value == 1:
+        return food
+    else:
+        food.set_cost(food.cost * food.rarity.value)
+        return food
+
+
+def randomize_drink():
+    name = random.choice(drinkNames)
+    cost = random.randint(MIN_COST, MAX_COST)
+    healing = random.randint(MINIMUM_FOOD_HEALING, MAX_FOOD_HEALING)
+    stamina = random.randint(MIN_STAMINA_HEALING, MAX_STAMINA_HEALING)
+    drink = Drink(name, cost, "No description properly serves this right.", healing, stamina)
+    if drink.rarity.value == 1:
+        return drink
+    else:
+        drink.set_cost(drink.cost * drink.rarity.value)
+        return drink
